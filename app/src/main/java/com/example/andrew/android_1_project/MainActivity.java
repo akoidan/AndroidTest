75bdad3da1faefe2ce9d346846fed6c435c198e4
deathangel908 <123asd123>
2016-09-03 19:06:36 +0300

    my first commit

---
+++ app/src/main/java/com/example/andrew/android_1_project/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.andrew.android_1_project;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView result;
+    private EditText secondOperand;
+    private EditText firstOperand;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        Button sumBtn = (Button) findViewById(R.id.summBtn);
+        secondOperand = (EditText) findViewById(R.id.secondOperandText);
+        firstOperand = (EditText) findViewById(R.id.firstOperandText);
+        result = (EditText) findViewById(R.id.resultText);
+        sumBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Integer resid = Integer.parseInt(firstOperand.getText().toString()) + Integer.parseInt(secondOperand.getText().toString());
+                result.setText(resid.toString());
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
